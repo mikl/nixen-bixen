3301e75f82cc1cbde988d959117688d9afdbe5af
Mikkel Hoegh <mikkel@hoegh.org>
2026-04-13 01:30:51 +0200

    Set up Home Manager

--- flake.nix
+++ flake.nix
@@ -2,7 +2,30 @@
   description = "Nix setup by Mikkel T. Hoegh";
 
   inputs = {
-    # Use stable nixpkgs as a base.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."mikl" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
 }
