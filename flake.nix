cbcd70b67f339df858d4bf3b00cd34cf7da0fc28
Mikkel T. Hoegh <mikkel@hoegh.org>
2026-04-18 23:02:19 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -3,12 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.eidolon = nixpkgs.lib.nixosSystem {
       modules = [
         ./configuration.nix
+
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.mikl = import ./home.nix;
+        }
       ];
     };
   };
