0f226737f322dcfef2bffe096ca2275f0abe499a
Mikkel T. Hoegh <mikkel@hoegh.org>
2026-04-18 23:02:08 +0200

    Configure Git with Delta
    Migrated existing configuration from dotfiles.

--- home.nix
+++ home.nix
@@ -104,7 +104,69 @@
   };
 
   programs.btop.enable = true;
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+  };
+
   programs.fastfetch.enable = true;
+
+  programs.git = {
+    enable = true;
+    settings = {
+      alias = {
+        br = "branch";
+        st = "status";
+        ci = "commit";
+        co = "checkout";
+        staged = "diff --cached";
+        oneline = "log --pretty=oneline";
+        llog = "log --date=local";
+        changes = "diff --name-status -r";
+        unadd = "update-index --force-remove";
+      };
+      color = {
+        interactive = "auto";
+        ui = "auto";
+      };
+      core = {
+        autocrlf = "false";
+        editor = "nvim";
+        hooksPath = ".githooks";
+        legacyheaders = "false";
+        pager = "delta";
+        quotepath = "false";
+      };
+      format.numbered = "auto";
+      delta.navigate = "true";
+      fetch.prune = "true";
+      github.user = "mikl";
+      init.defaultBranch = "master";
+      merge.conflictstyle = "zdiff3";
+      mergetool = {
+        keepBackup = "false";
+        prompt = "false";
+      };
+      pull = {
+        # Do fast-forward only merges on `git pull`
+        rebase = "false";
+        ff = "only";
+      };
+      push = {
+        default = "simple";
+        autoSetupRemote = "true";
+      };
+      rebase.autoStash = 1;
+      repack.usedeltabaseoffset = "true";
+      rerere.enabled = 1;
+      user = {
+        name = "Mikkel T. Hoegh";
+        email = "mikkel@hoegh.org";
+      };
+    };
+  };
+
   programs.lazydocker.enable = true;
   programs.tealdeer.enable = true;
 }
