e09cc20b60c1eb73b2d1cb6b77a5753a3d14c34f
Mikkel T. Hoegh <mikkel@hoegh.org>
2026-05-14 23:42:55 +0200

    Configure Ghostty with Nix

--- modules/features/linux-desktop/basis.nix
+++ modules/features/linux-desktop/basis.nix
@@ -24,7 +24,6 @@
       home.stateVersion = "25.11"; # Please read the comment before changing.
 
       home.packages = with pkgs; [
-        ghostty
         nixfmt
         obsidian
         tealdeer
@@ -43,6 +42,62 @@
       ];
 
       programs.fastfetch.enable = true;
+
+      programs.ghostty = {
+        enable = true;
+        enableFishIntegration = true;
+        installBatSyntax = true;
+        installVimSyntax = true;
+        settings = {
+          theme = "Eldritch";
+          font-family = "Iosevka Term Slab";
+          font-size = 11;
+          keybind = [
+            # macOS style keyboard shortcuts on Linux.
+            "super+a=select_all"
+            "super+c=copy_to_clipboard"
+            "super+d=new_split:right"
+            "super+k=clear_screen"
+            "super+n=new_window"
+            "super+q=quit"
+            "super+t=new_tab"
+            "super+v=paste_from_clipboard"
+            "super+w=close_surface"
+            "super+zero=reset_font_size"
+            "super+shift+[=previous_tab"
+            "super+shift+]=next_tab"
+          ];
+        };
+        themes = {
+          # Based on https://github.com/eldritch-theme/ghostty
+          Eldritch = {
+            background = "212337";
+            cursor-color = "37f499";
+            foreground = "ebfafa";
+            palette = [
+              "0=#21222c"
+              "1=#f9515d"
+              "2=#37f499"
+              "3=#e9f941"
+              "4=#9071f4"
+              "5=#f265b5"
+              "6=#04d1f9"
+              "7=#ebfafa"
+              "8=#7081d0"
+              "9=#f16c75"
+              "10=#69f8b3"
+              "11=#f1fc79"
+              "12=#a48cf2"
+              "13=#fd92ce"
+              "14=#66e4fd"
+              "15=#ffffff"
+            ];
+            selection-background = "bf4f8e";
+            selection-foreground = "ebfafa";
+          };
+        };
+      };
+
       programs.neovide.enable = true;
 
       programs.zen-browser = {
