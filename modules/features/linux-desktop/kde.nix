6ec8a489aeabfb93ea547481f2a9424f101c596e
Mikkel Hoegh <mikkel@hoegh.org>
2026-05-15 19:31:33 +0200

    Add more KDE packages

--- modules/features/linux-desktop/kde.nix
+++ modules/features/linux-desktop/kde.nix
@@ -7,12 +7,25 @@
     { pkgs, ... }:
     {
       home.packages = with pkgs.kdePackages; [
+        akregator
+        alligator
+        aurorae
         dolphin
+        dolphin-plugins
+        dragon # Media player.
+        falkon
+        filelight
         gwenview
         kate
+        kdf
+        kompare
         krdc
         marble
+        ocean-sound-theme
         okular
+        oxygen
+        oxygen-icons
+        oxygen-sounds
         plasma-thunderbolt
         spectacle
       ];
