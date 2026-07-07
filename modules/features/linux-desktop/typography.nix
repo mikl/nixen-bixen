d0e0da754484de43e147c70e6d90bf04185a35fd
Mikkel T. Hoegh <mikkel@hoegh.org>
2026-07-07 22:57:04 +0200

    Add Geist font

--- modules/features/linux-desktop/typography.nix
+++ modules/features/linux-desktop/typography.nix
@@ -1,12 +1,13 @@
 /**
   Typography set-up for Linux desktop.
 */
-{ self, inputs, ... }:
+{ ... }:
 {
   flake.homeModules.linuxDesktopTypography =
     { pkgs, ... }:
     {
       home.packages = with pkgs; [
+        geist-font
         (pkgs.iosevka-bin.override { variant = "Etoile"; })
         (pkgs.iosevka-bin.override { variant = "Slab"; })
         (pkgs.iosevka-bin.override { variant = "SGr-IosevkaTermSlab"; })
